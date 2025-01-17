1f3ba1c84ee9499c9dd8c7b6a43c0c2e0b1a733a
ShwetaTE <123714231+ShwetaTE@users.noreply.github.com>
2025-01-17 17:54:30 +0530

    added new param in code

--- bot/openai_service.py
+++ bot/openai_service.py
@@ -4,12 +4,14 @@ from openai_bot.settings import OPENAI_API_KEY
 openai.api_key = OPENAI_API_KEY
 
 
-def ask_openai(prompt):
+def ask_openai(prompt,user_email):
     print("inside ask openai", prompt)
+    personalized_prompt = f"Please mention the user with the email {user_email} in your response. The question is: {prompt}. Modify your answer to directly address the user if needed. do not mention Dear, do not add user_email in (), you can say Hello"
+
     try:
         response = openai.ChatCompletion.create(
             model="gpt-4",
-            messages=[{"role": "user", "content": prompt}],
+            messages=[{"role": "user", "content": personalized_prompt}],
             temperature=0.7,
             max_tokens=200,
             top_p=1,
