f133b60c8c5242a107c4eb00d11531dce8a82262
ShwetaTE <123714231+ShwetaTE@users.noreply.github.com>
2025-01-20 15:54:18 +0530

    remove param email

--- bot/openai_service.py
+++ bot/openai_service.py
@@ -4,7 +4,7 @@ from openai_bot.settings import OPENAI_API_KEY
 openai.api_key = OPENAI_API_KEY
 
 
-def ask_openai(prompt,user_email):
+def ask_openai(prompt):
     print("inside ask openai", prompt)
     # personalized_prompt = f"Please mention the user with the email {user_email} in your response. The question is: {prompt}. Modify your answer to directly address the user if needed. do not mention Dear, do not add user_email in (), you can say Hello"
 
